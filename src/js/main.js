ac976cc6318823e7eec7cd51cc1b898adc29202d
JankieQwQ <qwq@awaish.link>
2024-03-09 13:52:22 +0800

    [feat] bartending

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+class Bar {
+    constructor(version) {
+        this.version = version;
+    }
+
+    bartending() {
+        const music = new Audio('adf.wav');
+        music.loop = true;
+        music.playbackRate = 2;
+        music.play();
+    }
+}
