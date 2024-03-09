af16701caf03d895041a5a29b9b6821707bbfc47
JankieQwQ <qwq@awaish.link>
2024-03-09 15:03:14 +0800

    [feat] Update style

--- src/js/main.js
+++ src/js/main.js
@@ -31,22 +31,26 @@ function load(){
 }
 
 function writeHTMLasJS(){
-    document.write("    <script type=\"text\/javascript\" id=\"myhk\" src=\"https:\/\/myhkw.cn\/api\/player\/170996683669\" key=\"170996683669\" m=\"1\"><\/script>");
-    document.write("    <svg width=\"200\" height=\"200\" viewBox=\"-100 -100 200 200\">");
-    document.write("        <g stroke=\"black\" stroke-width=\"2\">");
-    document.write("            <circle cx=\"0\" cy=\"-45\" r=\"7\" fill=\"#4F6D7A\" \/>");
-    document.write("            <circle cx=\"0\" cy=\"50\" r=\"7\" fill=\"#F79257\" \/>");
-    document.write("            <path");
-    document.write("                d=\"");
-    document.write("                M -50 40");
-    document.write("                L -50 50");
-    document.write("                L 50 50");
-    document.write("                L 50 40");
-    document.write("                Q 40 40 40 10");
-    document.write("                C 40 -60 -40 -60 -40 10");
-    document.write("                Q -40 40 -50 40\"");
-    document.write("                fill=\"#FDEA96\"");
-    document.write("            \/>");
-    document.write("        <\/g>");
-    document.write("    <\/svg><a href=\"http:\/\/dd.ma\/bgOwnmAs\">Buy<\/a>");
+    var str="";
+    str += "    <script type=\"text\/javascript\" id=\"myhk\" src=\"https:\/\/myhkw.cn\/api\/player\/170996683669\" key=\"170996683669\" m=\"1\"><\/script>";
+    str += "    <svg width=\"200\" height=\"200\" viewBox=\"-100 -100 200 200\">";
+    str += "        <g stroke=\"black\" stroke-width=\"2\">";
+    str += "            <circle cx=\"0\" cy=\"-45\" r=\"7\" fill=\"#4F6D7A\" \/>";
+    str += "            <circle cx=\"0\" cy=\"50\" r=\"7\" fill=\"#F79257\" \/>";
+    str += "            <path";
+    str += "                d=\"";
+    str += "                M -50 40";
+    str += "                L -50 50";
+    str += "                L 50 50";
+    str += "                L 50 40";
+    str += "                Q 40 40 40 10";
+    str += "                C 40 -60 -40 -60 -40 10";
+    str += "                Q -40 40 -50 40\"";
+    str += "                fill=\"#FDEA96\"";
+    str += "            \/>";
+    str += "        <\/g>";
+    str += "    <\/svg><a href=\"http:\/\/dd.ma\/bgOwnmAs\">Buy<\/a>";
+    
+    var Div=document.getElementById("before");
+    Div.insertAdjacentHTML("beforeEnd",str);
 }
